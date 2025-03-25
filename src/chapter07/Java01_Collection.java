c4cf429875a7a65dc29ab0becb3239caaa6ed024
kevin <cxhil.kevin.gmail.com>
2025-03-25 17:24:30 +0800

    集合

---
+++ src/chapter07/Java01_Collection.java
@@ -0,0 +1,18 @@
+package chapter07;
+
+public class Java01_Collection {
+    public static void main(String[] args) throws Exception {
+    //TODO Collection 集合
+        //資料容器 容納資料
+        //JAVA有完整的集合框架 很多集合類+接口
+        //TODO 問題 什麼時候會需要用到集合?
+        //對"不確定個數"資料 進行 相同邏輯的處理時。
+        //TODO 問題 如果要容納資料，那跟陣列差別在哪?
+        //陣列在個數不確定下 ，不方便
+        //TODO 根據資料不同 集合分為兩大體系
+        //1.單一資料體系 Collection 接口定義相關規則
+        //2.成對出現體系 Map接口定義相關規則
+            //key&value(鍵值對) 根據key 找到 value
+
+    }
+}
