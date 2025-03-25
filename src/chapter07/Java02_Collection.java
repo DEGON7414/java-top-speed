a08850b511aa3c9421d67faf3cbc528040f0157b
kevin <cxhil.kevin.gmail.com>
2025-03-25 17:24:49 +0800

    集合接口

---
+++ src/chapter07/Java02_Collection.java
@@ -0,0 +1,17 @@
+package chapter07;
+
+public class Java02_Collection {
+    public static void main(String[] args) throws Exception {
+    //TODO Collection 集合
+       //TODO Collection接口
+        //常用子接口
+        // List : 按照插入順序保存資料，資料可以重複
+                    //具體實現 ArrayList LinkedList
+        // Set: 集，無序保存，資料不能重複
+                    //具體實現 HashSet
+        // Queue: 隊列
+                    //具體實現 ArrayBlockingQueue
+        //TODO Map接口
+        // 具體實現: hashMap hashTable
+    }
+}
